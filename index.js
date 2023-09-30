41d548245e356912f03c829bdb52e07c5f97f95a
caroline opiyo <opiyocaroline20@gmail.com>
2023-09-30 18:15:50 +0300

    phase-0-javascript-events-event-listening-lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// Define the addingEventListener function
 function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', (e)=> {
+      console.log("Click Me!");
+  });
 }
+
+// Call the function after it's defined
+addingEventListener();
